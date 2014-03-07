46ffc69399a9f5a9326826ffdede0a5e5f2335f4
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-03-07 18:11:13 +0900

    Revert "Trimmed the branch to only the rosping sub directory"
    This reverts commit a82fc1a39d15b5cd13eb7d39e60d08f209ff5b71.

---
+++ jsk_topic_tools/catkin.cmake
@@ -0,0 +1,51 @@
+# http://ros.org/doc/groovy/api/catkin/html/user_guide/supposed.html
+cmake_minimum_required(VERSION 2.8.3)
+project(jsk_topic_tools)
+
+find_package(catkin REQUIRED COMPONENTS topic_tools message_generation roscpp rostest)
+
+add_message_files(
+  FILES TopicInfo.msg
+)
+
+add_service_files(
+  FILES List.srv Update.srv
+)
+
+generate_messages()
+
+catkin_package(
+    DEPENDS
+    CATKIN_DEPENDS topic_tools message_runtime
+    INCLUDE_DIRS
+    LIBRARIES
+)
+
+#include_directories(${Boost_INCLUDE_DIRS})
+add_executable(topic_buffer_server src/topic_buffer_server.cpp)
+add_executable(topic_buffer_client src/topic_buffer_client.cpp)
+add_dependencies(topic_buffer_server ${PROJECT_NAME}_gencpp)
+add_dependencies(topic_buffer_client ${PROJECT_NAME}_gencpp)
+target_link_libraries(topic_buffer_server ${catkin_LIBRARIES})
+target_link_libraries(topic_buffer_client ${catkin_LIBRARIES})
+
+add_rostest(test/test_topic_buffer.test)
+add_rostest(test/test_topic_buffer_close_wait.test)
+add_rostest(test/test_topic_buffer_fixed_rate.test)
+add_rostest(test/test_topic_buffer_fixed_rate_and_update_rate.test)
+add_rostest(test/test_topic_buffer_update_rate.test)
+
+install(TARGETS topic_buffer_server topic_buffer_client
+  ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+  LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+  RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
+  )
+
+install(DIRECTORY include/${PROJECT_NAME}/
+  DESTINATION ${CATKIN_PACKAGE_INCLUDE_DESTINATION}
+  )
+
+install(DIRECTORY scripts launch test DESTINATION
+  ${CATKIN_PACKAGE_SHARE_DESTINATION}
+  USE_SOURCE_PERMISSIONS
+  )
