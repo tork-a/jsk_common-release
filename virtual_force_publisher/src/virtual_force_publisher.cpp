bf1083882cb9ad5e21cb1ab1093c314420c39647
Kei Okada <kei.okada@gmail.com>
2015-06-19 11:37:38 +0900

    Rebase from 'upstream'

--- virtual_force_publisher/src/virtual_force_publisher.cpp
+++ virtual_force_publisher/src/virtual_force_publisher.cpp
@@ -163,7 +163,23 @@ namespace virtual_force_publisher{
 		
 		jnt_to_jac_solver_->JntToJac(jnt_pos_, jacobian_);
 		jac_t = jacobian_.data.transpose();
-		jac_t_pseudo_inv =(jac_t.transpose() * jac_t).inverse() *  jac_t.transpose();
+		if ( jacobian_.columns() >= jacobian_.rows() ) {
+		  jac_t_pseudo_inv =(jac_t.transpose() * jac_t).inverse() *  jac_t.transpose();
+		} else {
+		  jac_t_pseudo_inv =jac_t.transpose() * ( jac_t *  jac_t.transpose() ).inverse();
+		}
+#if 1
+		{
+		  ROS_INFO("jac_t# jac_t : ");
+		  Eigen::Matrix<double,6,6> mat_i =  mat_i = jac_t_pseudo_inv * jac_t;
+		  for (unsigned int i = 0; i < 6; i++) {
+		    std::stringstream ss;
+		    for (unsigned int j=0; j<6; j++)
+		      ss << std::fixed << std::setw(8) << std::setprecision(4) << mat_i(j,i) << " ";
+		    ROS_INFO_STREAM(ss.str());
+                    }
+		}
+#endif
                 // f = - inv(jt) * effort
                 for (unsigned int j=0; j<6; j++)
                     {
@@ -215,19 +231,19 @@ namespace virtual_force_publisher{
                     for (unsigned int i = 0; i < jnt_pos_.rows(); i++) {
                         std::stringstream ss;
                         for (unsigned int j=0; j<6; j++)
-                            ss << jacobian_(j,i) << " ";
+                            ss << std::fixed << std::setw(8) << std::setprecision(4) << jacobian_(j,i) << " ";
                         ROS_INFO_STREAM(ss.str());
                     }
                     ROS_INFO("effort : ");
                     std::stringstream sstau;
                     for (unsigned int i = 0; i < tau.rows(); i++) {
-                        sstau << tau(i) << " ";
+                        sstau << std::fixed << std::setw(8) << std::setprecision(4) << tau(i) << " ";
                     }
                     ROS_INFO_STREAM(sstau.str());
                     ROS_INFO("force : ");
                     std::stringstream ssf;
                     for (unsigned int j = 0; j < 6; j++) {
-                        ssf << F(j) << " ";
+		      ssf << std::fixed << std::setw(8) << std::setprecision(4) << F(j) << " ";
                     }
                     ROS_INFO_STREAM(ssf.str());
                 }
