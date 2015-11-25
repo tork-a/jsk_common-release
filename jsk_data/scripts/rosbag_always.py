71a683a240511d837f8cb84dc8f952d09080758f
Kei Okada <kei.okada@gmail.com>
2015-11-25 22:31:06 +0900

    Rebase from 'upstream'

--- jsk_data/scripts/rosbag_always.py
+++ jsk_data/scripts/rosbag_always.py
@@ -9,6 +9,12 @@ import sys
 import argparse
 import re
 import shutil
+try:
+    import colorama
+except:
+    print "Please install colorama by pip install colorama"
+    sys.exit(1)
+from colorama import Fore, Style
 from jsk_topic_tools.master_util import isMasterAlive
 
 
@@ -16,12 +22,11 @@ def runROSBag(topics, size, save_dir):
     """
     run rosbag and return Popen object
     """
-    cmd = 'rosbag record --split'
+    cmd = 'roslaunch jsk_data rosbag_always_run_rosbag.launch'
     formatted_topics = [t for t in topics.split(' ') if t]
-    return subprocess.Popen(cmd.split(' ') 
-                            + formatted_topics 
-                            + ['--size', size]
-                            + ['-o', save_dir + '/rosbag'])
+    args = cmd.split(' ') + ["TOPICS:=" + topics + ""] + ["SIZE:=" + size] + ["OUTPUT:=" + save_dir + '/rosbag']
+    print args
+    return subprocess.Popen(args)
 
 def parseBagFile(bag):
     # bag file name is ...
@@ -76,7 +81,9 @@ def getDirectorySize(start_path = '.'):
 
 def keyFuncToSortBag(bag):
     parse = parseBagFile(os.path.basename(bag))
-    return int(reduce(lambda x, y: x + y, parse))
+    parse[len(parse) - 1] = str(int(parse[len(parse) - 1])).zfill(4)
+    concatenated_string = reduce(lambda x, y: x + y, parse)
+    return int(concatenated_string)
     
 def listBagsSortedByDate(save_dir):
     bags = []
@@ -91,16 +98,17 @@ def removeOldFiles(save_dir, max_size, current_size):
     remove_size = current_size - max_size
     for f in files:
         the_size = os.path.getsize(f)
-        print 'removing %s (%d)' % (f, the_size / 1000 / 1000)
+        print Fore.GREEN + 'removing %s (%d)' % (f, the_size / 1000 / 1000) + Fore.RESET
         os.remove(f)
+        # Send desktop notification
+        subprocess.check_output(['notify-send', "Removed %s (%d)" % (f, the_size / 1000 / 1000)])
         remove_size = remove_size - the_size / 1000.0 / 1000.0
         if remove_size < 0:
             return
     
 def checkDirectorySize(save_dir, max_size):
     size = getDirectorySize(save_dir)
-    print 'current directory size is %fM (max is %dM)' % (size, int(max_size))
-    
+    # print 'current directory size is %fM (max is %dM)' % (size, int(max_size))
     if size > max_size:
         removeOldFiles(save_dir, max_size, size)
     
@@ -126,8 +134,11 @@ def killROSBag():
     if g_rosbag_process:
         print 'Killing rosbag ...'
         rosbag_pid = g_rosbag_process.pid
-        killChildProcesses(rosbag_pid)
-        g_rosbag_process.send_signal(subprocess.signal.SIGINT)
+        try:
+            killChildProcesses(rosbag_pid)
+            g_rosbag_process.send_signal(subprocess.signal.SIGINT)
+        except:
+            pass
     
 def main(topics, size, save_dir, max_size, rate = 1):
     if not os.path.exists(save_dir):
@@ -145,9 +156,11 @@ def main(topics, size, save_dir, max_size, rate = 1):
             watchFileSystem(save_dir, max_size)
             previous_master_state = master_state
             time.sleep(1.0 / rate)
-    except:
+    except Exception, e:
         time.sleep(1)
         watchFileSystem(save_dir, max_size)
+    finally:
+        killROSBag()
 
 
         
