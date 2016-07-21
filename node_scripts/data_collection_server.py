8c9fbfdb778d2702c18640002f286ff799c3a43b
Kei Okada <kei.okada@gmail.com>
2016-07-21 11:47:56 +0900

    Rebase from 'debian/indigo/jsk_data'

--- node_scripts/data_collection_server.py
+++ node_scripts/data_collection_server.py
@@ -7,14 +7,20 @@ from __future__ import print_function
 import os
 import os.path as osp
 import pickle as pkl
+import sys
 
 import cv2
+import yaml
 
 import cv_bridge
+import dynamic_reconfigure.server
+from jsk_topic_tools.log_utils import jsk_logfatal
 import roslib.message
 import rospy
 from std_srvs.srv import Trigger
 from std_srvs.srv import TriggerResponse
+
+from jsk_data.cfg import DataCollectionServerConfig
 
 
 class DataCollectionServer(object):
@@ -40,12 +46,27 @@ class DataCollectionServer(object):
     """
 
     def __init__(self):
+        dynamic_reconfigure.server.Server(
+            DataCollectionServerConfig, self.reconfig_cb)
         self.msg = {}
-        self.save_dir = osp.expanduser(rospy.get_param('~save_dir', '~/.ros'))
-        if not osp.exists(self.save_dir):
-            os.makedirs(self.save_dir)
-        self.topics = rospy.get_param('~topics')
-        self.params = rospy.get_param('~params')
+        self.topics = rospy.get_param('~topics', [])
+        # validation for saving topics
+        for topic in self.topics:
+            required_fields = ['name', 'msg_class', 'fname', 'savetype']
+            for field in required_fields:
+                if field not in topic:
+                    jsk_logfatal("Required field '{}' for topic is missing"
+                                 .format(field))
+                    sys.exit(1)
+        self.params = rospy.get_param('~params', [])
+        # validation for saving params
+        for param in self.params:
+            required_fields = ['key', 'fname', 'savetype']
+            for field in required_fields:
+                if field not in topic:
+                    jsk_logfatal("Required field '{}' for param is missing"
+                                 .format(field))
+                    sys.exit(1)
         self.server = rospy.Service('~save_request', Trigger, self.service_cb)
         self.subs = []
         for topic in self.topics:
@@ -53,6 +74,12 @@ class DataCollectionServer(object):
             sub = rospy.Subscriber(topic['name'], msg_class, self.sub_cb,
                                    callback_args=topic['name'])
             self.subs.append(sub)
+
+    def reconfig_cb(self, config, level):
+        self.save_dir = osp.expanduser(config['save_dir'])
+        if not osp.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+        return config
 
     def __del__(self):
         for sub in self.subs:
@@ -83,16 +110,26 @@ class DataCollectionServer(object):
                 with open(osp.join(save_dir, topic['fname']), 'wb') as f:
                     pkl.dump(depth, f)
             else:
+                rospy.logerr('Unexpected savetype for topic: {}'
+                             .format(topic['savetype']))
                 raise ValueError
         for param in self.params:
             value = rospy.get_param(param['key'])
             if param['savetype'] == 'Text':
                 with open(osp.join(save_dir, param['fname']), 'w') as f:
                     f.write(str(value))
+            elif param['savetype'] == 'YAML':
+                content = yaml.safe_dump(value, allow_unicode=True,
+                                         default_flow_style=False)
+                with open(osp.join(save_dir, param['fname']), 'w') as f:
+                    f.write(content)
             else:
+                rospy.logerr('Unexpected savetype for param: {}'
+                             .format(param['savetype']))
                 raise ValueError
-        rospy.loginfo('Saved data to {}'.format(save_dir))
-        return TriggerResponse(success=True)
+        message = 'Saved data to {}'.format(save_dir)
+        rospy.loginfo(message)
+        return TriggerResponse(success=True, message=message)
 
 
 if __name__ == '__main__':
