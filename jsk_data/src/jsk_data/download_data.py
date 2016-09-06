e77549f78959399786f3411e3abe533caae32850
Kei Okada <kei.okada@gmail.com>
2016-09-06 09:26:29 +0900

    Rebase from 'upstream'

--- jsk_data/src/jsk_data/download_data.py
+++ jsk_data/src/jsk_data/download_data.py
@@ -78,14 +78,14 @@ def download_data(pkg_name, path, url, md5, download_client=None,
             download_client = 'wget'
     if compressed_bags is None:
         compressed_bags = []
-    # get package path
-    rp = rospkg.RosPack()
-    pkg_path = rp.get_path(pkg_name)
     if not osp.isabs(path):
+        # get package path
+        rp = rospkg.RosPack()
+        pkg_path = rp.get_path(pkg_name)
         path = osp.join(pkg_path, path)
     # prepare cache dir
     ros_home = os.getenv('ROS_HOME', osp.expanduser('~/.ros'))
-    cache_dir = osp.join(ros_home, 'data')
+    cache_dir = osp.join(ros_home, 'data', pkg_name)
     if not osp.exists(cache_dir):
         os.makedirs(cache_dir)
     cache_file = osp.join(cache_dir, osp.basename(path))
@@ -109,5 +109,8 @@ def download_data(pkg_name, path, url, md5, download_client=None,
     if extract:
         extract_file(path, to_directory=osp.dirname(path))
     for compressed_bag in compressed_bags:
-        compressed_bag = osp.join(pkg_path, compressed_bag)
+        if not osp.isabs(compressed_bag):
+            rp = rospkg.RosPack()
+            pkg_path = rp.get_path(pkg_name)
+            compressed_bag = osp.join(pkg_path, compressed_bag)
         decompress_rosbag(compressed_bag, quiet=quiet)
