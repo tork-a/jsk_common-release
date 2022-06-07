bef9a5a58d30fabde7e5208a597d705905655400
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2022-06-07 15:52:39 +0900

    Rebase from 'upstream'

--- jsk_topic_tools/src/jsk_topic_tools/transport.py
+++ jsk_topic_tools/src/jsk_topic_tools/transport.py
@@ -3,12 +3,14 @@
 import abc
 import argparse
 from distutils.version import LooseVersion
-import pkg_resources
 import sys
 
+from diagnostic_msgs.msg import DiagnosticStatus
+import pkg_resources
 import rospy
 
 from jsk_topic_tools.name_utils import unresolve_name
+from jsk_topic_tools import TimeredDiagnosticUpdater
 
 
 __all__ = ('ConnectionBasedTransport',)
@@ -48,6 +50,17 @@ class MetaConnectionBasedTransport(abc.ABCMeta):
         return obj
 
 
+class _Publisher(rospy.Publisher):
+
+    def __init__(self, *args, **kwargs):
+        super(_Publisher, self).__init__(*args, **kwargs)
+        self.last_published_time = rospy.Time.from_sec(0)
+
+    def publish(self, *args, **kwargs):
+        super(_Publisher, self).publish(*args, **kwargs)
+        self.last_published_time = rospy.Time.now()
+
+
 class ConnectionBasedTransport(rospy.SubscribeListener):
 
     __metaclass__ = MetaConnectionBasedTransport
@@ -58,6 +71,26 @@ class ConnectionBasedTransport(rospy.SubscribeListener):
         self._publishers = []
         self._ever_subscribed = False
         self._connection_status = NOT_SUBSCRIBED
+
+        if rospy.get_param('~enable_vital_check', True):
+            self.diagnostic_updater = TimeredDiagnosticUpdater(
+                rospy.Duration(1.0))
+            self.node_name = rospy.get_name()
+            self.diagnostic_updater.set_hardware_id(self.node_name)
+            self.diagnostic_updater.add(self.node_name,
+                                        self.update_diagnostic)
+
+            self.use_warn = rospy.get_param(
+                '/diagnostic_nodelet/use_warn',
+                rospy.get_param('~use_warn', False))
+            if self.use_warn:
+                self.diagnostic_error_level = DiagnosticStatus.WARN
+            else:
+                self.diagnostic_error_level = DiagnosticStatus.ERROR
+            self.vital_rate = rospy.get_param('~vital_rate', 1.0)
+            self.dead_duration = 1.0 / self.vital_rate
+            self.diagnostic_updater.start()
+
         kwargs = dict(
             period=rospy.Duration(5),
             callback=self._warn_never_subscribed_cb,
@@ -88,6 +121,15 @@ class ConnectionBasedTransport(rospy.SubscribeListener):
                 " child subscribers. Set '~always_subscribe' as True"
                 ' to have it subscribe always.'.format(name=rospy.get_name()))
 
+    def _is_running(self):
+        current_time = rospy.Time.now()
+        # check subscribed topics are published.
+        return all([
+            (current_time.to_sec() - pub.last_published_time.to_sec())
+            < self.dead_duration
+            for pub in self._publishers
+            if pub.get_num_connections() > 0])
+
     @abc.abstractmethod
     def subscribe(self):
         raise NotImplementedError
@@ -99,11 +141,25 @@ class ConnectionBasedTransport(rospy.SubscribeListener):
     def is_subscribed(self):
         return self._connection_status == SUBSCRIBED
 
+    def poke(self):
+        """Update the time of last_published_time.
+
+        Update the time of last_published_time
+        to make it possible to take the difference time
+        between the time of start subscribing and the current time.
+        """
+        start_time = rospy.Time.now()
+        for pub in self._publishers:
+            pub.last_published_time = start_time
+
     def peer_subscribe(self, *args, **kwargs):
         rospy.logdebug('[{topic}] is subscribed'.format(topic=args[0]))
         if self._connection_status == NOT_SUBSCRIBED:
             self.subscribe()
             self._connection_status = SUBSCRIBED
+
+            self.poke()
+
             if not self._ever_subscribed:
                 self._ever_subscribed = True
 
@@ -127,6 +183,27 @@ class ConnectionBasedTransport(rospy.SubscribeListener):
         assert kwargs.get('subscriber_listener') is None
         kwargs['subscriber_listener'] = self
 
-        pub = rospy.Publisher(*args, **kwargs)
+        pub = _Publisher(*args, **kwargs)
         self._publishers.append(pub)
         return pub
+
+    def update_diagnostic(self, stat):
+        if self._connection_status == SUBSCRIBED:
+            if self._is_running():
+                stat.summary(
+                    DiagnosticStatus.OK,
+                    '{} is running'.format(self.node_name))
+            else:
+                stat.summary(
+                    self.diagnostic_error_level,
+                    "{} is not running for {} sec".format(
+                        self.node_name, self.dead_duration))
+        else:
+            stat.summary(
+                DiagnosticStatus.OK,
+                '{} is not subscribed'.format(self.node_name))
+        topic_names = ', '.join([pub.name for pub in self._publishers])
+        stat.add('watched topics', topic_names)
+        for pub in self._publishers:
+            stat.add(pub.name, '{} subscribers'.format(
+                pub.get_num_connections()))
